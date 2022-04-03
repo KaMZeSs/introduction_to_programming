dba8cd52c6e864b27385ff3989588601b3750a39
KaMZeSs <83509865+KaMZeSs@users.noreply.github.com>
2022-04-03 18:57:59 +0300

    Add files via upload

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.папкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.папкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // папкаToolStripMenuItem
+            // 
+            this.папкаToolStripMenuItem.Name = "папкаToolStripMenuItem";
+            this.папкаToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.папкаToolStripMenuItem.Text = "Папка";
+            this.папкаToolStripMenuItem.Click += new System.EventHandler(this.папкаToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Введите число";
+            this.textBox1.Size = new System.Drawing.Size(800, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(0, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(800, 403);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem папкаToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ Lab4/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void папкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0 | textBox1.ForeColor == Color.Red)
+            {
+                MessageBox.Show("Невозможно считать число");
+                return;
+            }
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Выберите папку";
+            folderBrowserDialog.UseDescriptionForTitle = true;
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                try 
+                {
+                    String[] files = Directory.GetFiles(folderBrowserDialog.SelectedPath, "*.*", SearchOption.AllDirectories).
+                        Where(file => file.ToLower().EndsWith(".c") | file.ToLower().EndsWith(".cpp")).ToArray();
+
+                    Dictionary<String, int> dict = new Dictionary<string, int>();
+                    foreach (String file in files)
+                    {
+                        using (StreamReader reader = new StreamReader(file))
+                        {
+                            String text = reader.ReadToEnd();
+                            Regex regex = new Regex(@"{|}");
+                            Match[] matches = regex.Matches(text).ToArray();
+                            Char[] letters = new char[matches.Length];
+                            for (int i = 0; i < matches.Length; i++)
+                            {
+                                letters[i] = matches[i].ToString()[0];
+                            }
+
+                            if (Verification(letters) & CountNesting(letters) > Convert.ToInt32(textBox1.Text))
+                            {
+                                if (((DateTime.Now - File.GetCreationTime(file)).TotalDays / 365.2425) >= 2)
+                                {
+                                    listBox1.Items.Add($"{file} - удален");
+                                    File.Delete(file);
+                                }
+                                else
+                                    listBox1.Items.Add(file);
+                            }
+                        }
+                    }
+
+                    textBox1.Text = listBox1.Items.Count.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Непредвиденная ошибка");
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private bool Verification(Char[] chars)
+        {
+            Stack<Char> stack = new Stack<Char>();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '{')
+                    stack.Push(chars[i]);
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    else
+                        stack.Pop();
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        private int CountNesting(Char[] chars)
+        {
+            int max = 0;
+
+            for (int i = 0, cur = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '{')
+                {
+                    cur++;
+                    max = max < cur ? cur : max;
+                }
+                else if (chars[i] == '}')
+                    cur--;
+
+            }
+
+            return max;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox1.Text, out int val))
+            {
+                textBox1.ForeColor = Color.Black;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+        }
+    }
+}
